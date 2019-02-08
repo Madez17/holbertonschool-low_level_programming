76efba36e2f77d23aa2ef9b07658306393af2b9b
Madez17 <781@holbertonschool.com>
2019-02-08 23:56:02 +0000

    print number base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+
+	int b10;
+
+	for (b10 = 0; b10 <= 9; b10++)
+	{
+		printf("%d", b10);
+	}
+	putchar ('\n');
+	return (0);
+}
