97780f30b1a47f8e91e444ac100a5f695d2d417b
Madez17 <781@holbertonschool.com>
2019-02-12 19:45:23 +0000

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+* main - check the code for Holberton School students.
+*
+* Return: Always 0.
+*/
+void print_alphabet(void)
+{
+	char az;
+
+	for (az = 'a'; az <= 'z'; az++)
+	{
+		_putchar(az);
+	}
+	_putchar('\n');
+}
