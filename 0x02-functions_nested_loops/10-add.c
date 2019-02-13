07a9813f11e3f35762a2469123fb02fb97024c10
Madez17 <781@holbertonschool.com>
2019-02-13 02:47:22 +0000

    Last digit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+* add - check the code for Holberton School students.
+*
+* @n: Es un valor entero
+*
+* @m: Es un valor entero
+*
+* Return: Always 0.
+*/
+int add(int n, int m)
+{
+	int c = n + m;
+	return (c);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+* print_last_digit - check the code for Holberton School students.
+*
+* @n: Valor 
+*
+* Return: Always 0.
+*/
+int print_last_digit(int n)
+{
+	n = n % 10;
+	if (n < 0)
+	{
+		n = n * -1;
+	}
+	_putchar(n + '0');
+	return (n);
+}
