099f3160e42e8251d51b6c111428ba3158547164
Madez17 <Madez17>
2019-02-14 19:49:45 +0000

    Mul two integers

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+* mul - check the code for Holberton School students.
+*
+* @a: Valor number
+*
+* @b: Valor number
+*
+* Return: Always 0.
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
