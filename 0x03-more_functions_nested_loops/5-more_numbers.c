ca4115ec4dcb5659f6049767a5a7ceaa16c015f6
Madez17 <Madez17>
2019-02-14 23:08:51 +0000

    Print numbers 0 to 14

---
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+* more_numbers - check the code for Holberton School students.
+*
+* Return: Always 0.
+*/
+void more_numbers(void)
+{
+	int times = 0;
+	int nums;
+	
+	while (times <= 9)
+	{
+		for (nums = 0; nums <= 14; nums++)
+		{
+			if(nums > 9 )
+			{
+				_putchar(nums / 10  + '0');
+			}
+			_putchar(nums % 10 + '0');
+		}
+		times++;
+		_putchar('\n');
+	}
+	nums = 0;
+}
