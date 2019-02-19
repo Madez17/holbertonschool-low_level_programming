b8bdf7568cedd3671277d61f15afb49ab54f1a94
Madez17 <Madez17>
2019-02-19 20:39:27 +0000

    Exchange value of a and b

--- 0x04-pointers_arrays_strings/1-swap.c
+++ 0x04-pointers_arrays_strings/1-swap.c
@@ -1,16 +1,18 @@
 #include "holberton.h"
 
 /**
-* swap_int - Reasigned value
+* swap_int - Exchange the value
 *
-* @n: This is a pointers
+* @a: Value 98
+*
+* @b: Value 42
 *
 * Return: Always 0.
 */
 void swap_int(int *a, int *b)
 {
 	int z;
-	
+
 	z = *a;
 	*a = *b;
 	*b = z;
