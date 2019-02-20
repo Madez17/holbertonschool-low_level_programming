fbd38fbee70617f40e1de845bcabe600fbbba767
Madez17 <Madez17>
2019-02-20 22:22:52 +0000

    Reverse string

---
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+  * rev_string - Function that reverse a string 
+  *
+  * @s: String Holberton
+  *
+ */
+void rev_string(char *s)
+{
+	int der = 0; /*Posicion inicial 0(en este caso) de mi string 'H'*/
+	int izq = 8; /*Posicion final 8(en este caso) de mi string 'n'*/
+	int aux; /*Variable externa me guarda el valor que va a ser cambiado*/
+
+	while (der < izq)
+	{
+		aux = *(s + der);
+		*(s + der) = *(s + izq);
+		*(s + izq) = aux;
+		der++;
+		izq--;
+	}
+}
