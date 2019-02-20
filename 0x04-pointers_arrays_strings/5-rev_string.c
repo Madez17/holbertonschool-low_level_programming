7d04353ec83a657ecb9e3534e1b8b4d59a5206e2
Madez17 <Madez17>
2019-02-20 22:48:30 +0000

    add code length calculate

--- 0x04-pointers_arrays_strings/5-rev_string.c
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -1,7 +1,8 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
-  * rev_string - Function that reverse a string 
+  * rev_string - Function that reverse a string
   *
   * @s: String Holberton
   *
@@ -9,9 +10,13 @@
 void rev_string(char *s)
 {
 	int der = 0; /*Posicion inicial 0(en este caso) de mi string 'H'*/
-	int izq = 8; /*Posicion final 8(en este caso) de mi string 'n'*/
+	int izq; /*Posicion final 8(en este caso) de mi string 'n'*/
 	int aux; /*Variable externa me guarda el valor que va a ser cambiado*/
 
+	for (izq = 0; *(s + izq) != '\0'; izq++)
+	{
+	}
+	izq--;
 	while (der < izq)
 	{
 		aux = *(s + der);
