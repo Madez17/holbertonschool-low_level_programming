f847ade9d6d70efb887a9307ed0c937b0b1958cf
Madez17 <Madez17>
2019-02-20 02:50:13 +0000

    Reverse

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+* print_rev - Function string var
+*
+* @s: String Holberton
+*
+* Return: Always 0.
+*/
+void print_rev(char *s)
+{
+	int strlong;
+
+	while (s[strlong])
+		strlong++;
+	while(strlong--)
+		_putchar(s[strlong]);
+}
