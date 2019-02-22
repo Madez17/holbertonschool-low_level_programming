bd83167c1f161341b30d98f646b0f5b401746ad0
Madez17 <Madez17>
2019-02-22 23:44:23 +0000

    Reverse array

---
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+  * _strcmp - concatenacion.
+  *
+  * @s1: dest array
+  *
+  * @s2: array
+  *
+  * Return: Always 0.
+  */
+void reverse_array(int *a, int n)
+{
+
+}
