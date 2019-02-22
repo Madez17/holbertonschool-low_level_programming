6346294a13f3d24e10d7ca42bf3858ee1900f07c
Madez17 <Madez17>
2019-02-22 21:29:24 +0000

    Copy value to one var to other

---
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+  * _strncat - concatenacion.
+  *
+  * @dest: dest array
+  *
+  * @src: array
+  *
+  * @n: position
+  *
+  * Return: Always 0.
+  */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char cont;
+
+	for (cont = 0; *(src + cont) != '\0' && cont < n; cont++)
+	{
+		*(dest + cont) = *(src + cont);
+
+	}
+	for (; cont < n; cont++)
+		*(dest + cont) = '\0';
+	return (dest);
+}
