a3767d71e1d4be5560ded6947cdf1797eae1df44
Madez17 <Madez17>
2019-02-25 21:06:11 +0000

    copies memory area.

--- 0x06-pointers_arrays_strings/1-memcpy.c
+++ 0x06-pointers_arrays_strings/1-memcpy.c
@@ -15,10 +15,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int cont;
 
-	for (cont = 0; cont <= n; cont++)
+	for (cont = 0; cont < n; cont++)
 	{
 		*(dest + cont) = *(src + cont);
 	}
 	return (dest);
-
 }
