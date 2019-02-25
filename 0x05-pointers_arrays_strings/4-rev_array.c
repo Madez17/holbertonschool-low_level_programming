e87e08651c9af5c65ae4ca8ca266f398107a512d
Madez17 <781@holbertonschool.com>
2019-02-25 01:57:16 +0000

    reverse

--- 0x05-pointers_arrays_strings/4-rev_array.c
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -1,15 +1,28 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
-  * _strcmp - concatenacion.
+  * reverse_array- chage posicion elements into array.
   *
-  * @s1: dest array
+  * @a: Array elements
   *
-  * @s2: array
+  * @n: Lenght array
   *
-  * Return: Always 0.
   */
 void reverse_array(int *a, int n)
 {
+	int cont;
+	int lenght;
+	int save;
+	int medio;
 
+	lenght = n - 1;
+	medio = n / 2;
+
+	for (cont = 0; cont < medio; cont++)
+	{
+		save = *(a + lenght);
+		*(a + lenght) = *(a + cont);
+		*(a + cont) = save;
+		lenght--;
+	}
 }
