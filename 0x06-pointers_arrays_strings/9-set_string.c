648790f2f84c6f630ebddfb8faf9ee74fe0bd240
Madez17 <Madez17>
2019-02-26 03:34:31 +0000

    searches a string for any of a set of bytes

---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _memcpy- memory copy
+ *
+ * @dest: Array of elements copy.
+ *
+ * @n: bytes of the memory
+ *
+ * Return: String Uppercase
+ */
+void set_string(char **s, char *to)
+{
+	_putchar(s);
+}
