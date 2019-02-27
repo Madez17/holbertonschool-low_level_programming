fb13d52f0b7e5def5efcd666085bb9c1476bbcbc
Madez17 <Madez17>
2019-02-27 00:12:45 +0000

    un comit mas

---
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+* _strpbrk - Serch substring
+*
+* @_haystack: Value string
+*
+* @needle: string
+*
+* Return: beginning of the located substring.
+*/
+void print_diagsums(int size)
+{
+	int horizontal;
+	int result;
+	int position;
+	int lenght = size * size;
+
+	for (horizontal = 0; horizontal < lenght; horizontal++)
+	{
+		por
+
+	}
+}
