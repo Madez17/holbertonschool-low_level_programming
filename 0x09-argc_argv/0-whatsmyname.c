6711362f94364313a8f97da3eee87b9ab5014f3e
Madez17 <Madez17>
2019-03-02 23:44:05 +0000

    program that prints its nam

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 /**
  * main - my function
+ *
  * @argc: Numero argumentos
+ *
  * @argv: array unidimensional
+ *
  * Return: 0
  */
-int main (int argc, char ** argv) 
+int main(int argc, char **argv)
 {
 	(void)argc;
 	printf("%s\n", *argv);
-	return(0);
+	return (0);
 }
