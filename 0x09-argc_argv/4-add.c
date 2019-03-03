2b54ff4efafb60cbd96b4eca6fb611acde7914de
Madez17 <Madez17>
2019-03-03 03:39:54 +0000

    Adds positive numbers

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -12,18 +12,24 @@
 int main(int argc, char **argv)
 {
 	int cont;
+	int iteravalorarg;
 	int suma = 0;
+
 
 	for (cont = 1; cont < argc; cont++)
 	{
+		for (iteravalorarg = 0; argv[cont][iteravalorarg]; iteravalorarg++)
+		{
+			if (isdigit(argv[cont][iteravalorarg]) == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
 		if (isdigit(*argv[cont]) != 0)
 		{
 			suma = suma + atoi(argv[cont]);
-		}
-		else
-		{
-			printf("Error\n");
-			return (1);
 		}
 	}
 	printf("%d\n", suma);
