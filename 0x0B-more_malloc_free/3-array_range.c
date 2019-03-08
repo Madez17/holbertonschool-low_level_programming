1cdc77b7d2fa571a8348acf8f89498001bc8249d
Madez17 <Madez17>
2019-03-08 00:26:04 +0000

    Array of integers.

---
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * array_range - Array.
+ *
+ * @min: min value.
+ *
+ * @max: max value.
+ *
+ * Return: my pointer.
+ */
+
+int *array_range(int min, int max)
+{
+	int *p;
+	int cont;
+	int size = max - min + 1;
+
+	p = malloc(sizeof(int) * size);
+
+	if (!p)
+	{
+		return (NULL);
+	}
+	if (min > max)
+	{
+		return (NULL);
+	}
+	for (cont = 0; min <= max; cont++)
+	{
+		p[cont] = min;
+		min++;
+	}
+
+	return (p);
+
+
+}
