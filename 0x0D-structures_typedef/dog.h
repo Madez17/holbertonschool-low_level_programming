2568619432662ff5b5a989caa6e62923118f11ce
Madez17 <Madez17>
2019-03-12 00:41:55 +0000

    type struct dog

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * free_dog - Data type
+ *
+ * @char: Name.
+ * @float: age.
+ * @char: owner.
+ */
+struct dog
+{
+	char * name;
+	float age;
+	char * owner;
+};
+#endif
+
