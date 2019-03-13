396e152ab9b4f8c7c6afebe1480d95232f219894
Madez17 <Madez17>
2019-03-13 22:27:15 +0000

    Iter each elements of array

---
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function
+ *
+ * @array: Array.
+ * @size: lenght string.
+ * @action: pointer to a function.
+ *
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int cont;
+
+	for (cont = 0; cont < size; cont++)
+	{
+		(*action)(array[cont]);
+	}
+}
