babb8ca9e32769a6cf2ab3cf0ce71315f9fa2ca7
Madez17 <Madez17>
2019-03-15 22:16:54 +0000

    sum of all its parameters

--- 0x0F-variadic_functions/0-sum_them_all.c
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -2,23 +2,24 @@
 #include <stdarg.h>
 
 /**
- * sum_them_all - Suma n parameters tha pass.
+ * sum_them_all - Suma n parameters tha pass
+ *
+ * @n: Cantidad parameters.
  *
  * Return: suma.
  */
 int sum_them_all(const unsigned int n, ...)
-{	
+{
 	int suma = 0;
 	unsigned int cont;
 	va_list why;
-	
+
 	if (n == 0)
 	{
 		return (0);
 	}
-	
+
 	va_start(why, n);
-	
 	for (cont = 0; cont < n; cont++)
 	{
 		suma = va_arg(why, int) + suma;
