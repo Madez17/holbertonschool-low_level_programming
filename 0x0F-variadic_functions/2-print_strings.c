5b93a1edbe5907e0ad6b595a27b1137eebf5ac02
Madez17 <Madez17>
2019-03-16 21:02:08 +0000

    [Feature]  prints strings

---
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -0,0 +1,40 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings -
+ *
+ * Return: 
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list string;
+	unsigned int cont;
+	char *p;
+
+	va_start(string, n);
+
+	if (separator != NULL)
+	{
+		for (cont = 0; cont < n; cont++)
+		{
+			p = va_arg(string, char *);
+			
+			if (p == NULL)
+			{
+				printf("%p", p);
+			}
+
+			if (p != NULL)
+			{
+				printf("%s", p);
+			}
+			if (cont != n - 1)
+			{
+				printf("%s", separator);
+			}
+			
+	
+		}
+		printf("\n");
+	}
+}
