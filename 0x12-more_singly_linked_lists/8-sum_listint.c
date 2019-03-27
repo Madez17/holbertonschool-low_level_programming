942c38e21d280da008904724ef2f9005771515cf
Madez17 <Madez17>
2019-03-27 02:00:54 +0000

    Sum all nodes

--- 0x12-more_singly_linked_lists/8-sum_listint.c
+++ 0x12-more_singly_linked_lists/8-sum_listint.c
@@ -1,11 +1,27 @@
 #include "lists.h"
 
 /**
- * sum_listint -
+ * sum_listint - Function sum others nodes
  *
- * Return: 
+ * @head: pointer to structure.
+ *
+ * Return: sum
  */
 int sum_listint(listint_t *head)
 {
+	int sum = 0;
+	listint_t *aux;
 
+	if (head == NULL)
+		return (0);
+
+	aux = head;
+
+	while (aux != NULL)
+	{
+
+		sum = aux->n + sum;
+		aux = aux->next;
+	}
+	return (sum);
 }
