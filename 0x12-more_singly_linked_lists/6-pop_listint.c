b0ed4b69d2a663760e6b8df2ee942c1680b46845
Madez17 <Madez17>
2019-03-27 00:03:42 +0000

    delete first node

--- 0x12-more_singly_linked_lists/6-pop_listint.c
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -1,11 +1,29 @@
 #include "lists.h"
 
 /**
- * pop_listint -
+ * pop_listint - function delete first node
  *
- * Return: 
+ * @head: pointer to pointer
+ * Return: n
  */
 int pop_listint(listint_t **head)
 {
+	listint_t *aux;
+	int n;
+
+	if (*head == NULL)
+		return (0);
+
+	aux = *head;
+
+	if (aux != NULL)
+	{
+		*head = aux->next;
+		n = aux->n;
+		free(aux);
+		aux = NULL;
+	}
+	return (n);
+
 
 }
