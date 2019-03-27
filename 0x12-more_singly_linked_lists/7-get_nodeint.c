9c6ed2069de4e1a4bb145a3db986f0bed8ac60e4
Madez17 <Madez17>
2019-03-27 01:14:06 +0000

    Search index and return data of node

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -1,11 +1,28 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index -
+ * get_nodeint_at_index - Function search index and return value.
  *
- * Return: 
+ * @head: pointer to structure
+ * @index: Number of position
+ *
+ * Return: NULL
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+	listint_t *aux;
+	unsigned int iter = 0;
 
+	aux = head;
+
+	while (aux != NULL)
+	{
+		if (iter == index)
+		{
+			return (aux);
+		}
+		iter++;
+		aux = aux->next;
+	}
+	return (NULL);
 }
