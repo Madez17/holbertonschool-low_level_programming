c649a5fc7628f1fff11ce5f8fa6597bf82154205
Madez17 <Madez17>
2019-03-28 23:32:59 +0000

    Init commit

---
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - convert binary number to integer
+ *
+ * @b: string
+ *
+ * Return: save
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int iter = 0;
+	unsigned int b_number = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[iter])
+	{
+		b_number = b_number << 1;
+		b_number |= b[iter] - '0';
+		iter++;
+	}
+	return (b_number);
+}
