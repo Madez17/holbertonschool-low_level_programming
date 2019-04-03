b3d6bca519b646fbd2c25de4612644ba94480fc4
Madez17 <Madez17>
2019-04-03 02:39:02 +0000

    one exercise

---
+++ 0x14-file_io/2-append_text_to_file.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * append_text_to_file - create a file
+ *
+ * @filename: name file to create
+ * @text_content: content to file
+ *
+ * Return: 1
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int iter;
+	int fd;
+	
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREATE | O_TRUNC | O_RDONLY | O_WRONLY, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		return (0);
+
+	while (text_content[iter] != '\0')
+		iter++;
+
+	if (write(fd, text_content, iter) < 0)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
