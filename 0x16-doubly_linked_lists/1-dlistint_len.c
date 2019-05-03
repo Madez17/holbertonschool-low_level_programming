6dc2bcbb17ca68fd7c63e2994f03aa1f8e0ac51f
Madez17 <Madez17>
2019-05-03 23:56:22 +0000

    First repository

---
+++ 0x16-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * dlistint_len -
+ *
+ * Return: 
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t iter = 0;
+
+	for (; h != NULL; h = h->next)
+	{
+		if (h)
+	}
+}
