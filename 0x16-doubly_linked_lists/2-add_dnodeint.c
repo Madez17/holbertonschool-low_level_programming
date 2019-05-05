9ff4d3634eb06a91111abbb19e30a8027e46f5dc
Madez17 <781@holbertonschool.com>
2019-05-05 22:37:35 +0000

    Fixed exercise, add node to the begining

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -17,14 +17,15 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	}
 
+	nAux->n = n;
+	nAux->next = *head;
+	nAux->prev = NULL;
+
 	if (*head == NULL)
 	{
 		(*head) = nAux;
 		return (*head);
 	}
-	nAux->n = n;
-	nAux->next = *head;
-	nAux->prev = NULL;
 	(*head)->prev = nAux;
 	*head = nAux;
 	return (nAux);
