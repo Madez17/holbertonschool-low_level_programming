06b85d1b4f5fff93b379a1f9714cb302abf0c9a3
Madez17 <781@holbertonschool.com>
2019-05-06 04:48:52 +0000

    Function - Sum all nodes

--- 0x16-doubly_linked_lists/6-sum_dlistint.c
+++ 0x16-doubly_linked_lists/6-sum_dlistint.c
@@ -1,11 +1,20 @@
 #include "lists.h"
 
 /**
- * sum_dlistint -
+ * sum_dlistint - Function that sum all nodes
  *
- * Return: 
+ * @head: Pointer to the initial node
+ * Return: Sum that nodes
  */
 int sum_dlistint(dlistint_t *head)
 {
+	int sum = 0;
+	dlistint_t *aux = head;
 
+	while (aux != NULL)
+	{
+		sum += aux->n;
+		aux = aux->next;
+	}
+	return (sum);
 }
