21f8a49fe5f0a55eea5bdc597d44dff7649d8031
Madez17 <781@holbertonschool.com>
2019-05-06 01:14:31 +0000

    Free allocs

--- 0x16-doubly_linked_lists/4-free_dlistint.c
+++ 0x16-doubly_linked_lists/4-free_dlistint.c
@@ -1,11 +1,14 @@
 #include "lists.h"
 
 /**
- * free_dlistint -
- *
- * Return: 
+ * free_dlistint - Function free allocs
+ * @head: head pointer to initial node
  */
 void free_dlistint(dlistint_t *head)
 {
-
+	while (head != NULL)
+	{
+		free(head);
+		head = head->next;
+	}
 }
