b50b710e69f4a083837cec747ad4135bbe3a049a
Madez17 <781@holbertonschool.com>
2019-05-06 03:05:03 +0000

    Function that return the specific node given with the variable index

--- 0x16-doubly_linked_lists/5-get_dnodeint.c
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -1,11 +1,23 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index -
+ * get_dnodeint_at_index - Function Iter index and print this.
  *
- * Return: 
+ * @head: Pointer First(node initial)
+ * @index: Number of node.
+ * Return: aux 
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-
+	dlistint_t *aux = head;
+	unsigned int iter2 = 0;
+	
+	if (head == NULL)
+		return (NULL);
+	while (iter2 != index)
+	{
+		aux = aux->next;
+		iter2++;
+	}
+	return (aux);
 }
