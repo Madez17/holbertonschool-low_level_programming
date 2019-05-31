2be35daf3b2c3c2d815d21fe0f0a117f7d8c44d0
Madez17 <Madez17>
2019-05-31 22:32:54 +0000

    Add element to the hash table

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -7,5 +7,33 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+    unsigned long int index;
+    const unsigned char *keynew;
+    hash_node_t *newNode;
+    hash_node_t *aux;
 
+    aux = ht->array[index];
+    keynew = (const unsigned char *)key;
+    index = key_index(keynew, ht->size);
+
+
+    if (ht == NULL || value == NULL || key == NULL)
+        return (0);
+   
+    while (aux != NULL)
+    {
+        if (strcmp(key,aux->key) == 0)
+        {
+            newNode->value = strdup(value);
+            return (1);
+        }
+        aux = aux->next;
+    }
+    newNode = malloc(sizeof(hash_node_t*));
+    newNode->key = strdup(key);
+    newNode->value = strdup(value);
+    newNode->next = aux;
+    aux = newNode;
+    ht->array[index] = newNode;
+    return (1);
 }
