23fd4f260c44e856017f873e518d1224b5782344
Madez17 <Madez17>
2019-05-31 00:36:46 +0000

    update

---
+++ 0x19-hash_tables/.#0-hash_table_create.c
@@ -0,0 +1,1 @@
+vagrant@vagrant-ubuntu-trusty-64.3609:1559250767
--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -3,22 +3,26 @@
 /**
  * hash_table_create - Function that create a hash table
  *
+ * @size: size of array
+ *
  * Return: Hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *hash_new;
-    hash_new = malloc(sizeof(hash_table_t *));
-    if(hash_new == NULL)
-    {
-        return NULL;
-    }
-    hash_new->size = size;
-    hash_new->array = malloc(sizeof(hash_node_t *) * size);
-    if(hash_new->array == NULL)
-    {
-        return NULL;
-    }
-    return hash_new;
-    free(hash_new);
+hash_table_t *hash_new;
+hash_new = malloc(sizeof(hash_table_t *));
+
+if (hash_new == NULL)
+{
+return (NULL);
+}
+hash_new->size = size;
+hash_new->array = malloc(sizeof(hash_node_t *) * size);
+
+if (hash_new->array == NULL)
+{
+free(hash_new);
+return (NULL);
+}
+return (hash_new);
 }
