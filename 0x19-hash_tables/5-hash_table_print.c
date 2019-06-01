6de0bba11e867f25266ed2b149d3be3adcd19682
Madez17 <Madez17>
2019-06-01 02:59:36 +0000

    print dictionary format

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -7,5 +7,31 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
+    unsigned long int iter_array = 0, p = 1;
+    hash_node_t *aux;
+    
+    printf("{");
+    while (iter_array < ht->size)
+    {
+        aux = ht->array[iter_array];
+
+        while (aux != NULL)
+        {
+            if (p == 1)
+            {
+                printf("'%s':'%s'", aux->key, aux->value);
+                aux = aux->next;
+                p = 0;
+            }
+            else
+            {
+                printf(",'%s':'%s'", aux->key, aux->value);
+                aux = aux->next;
+
+            }
+        }
+        iter_array++;
+    }
+    printf("}\n");
 
 }
