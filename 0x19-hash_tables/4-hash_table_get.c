74b10bb026b8eb8cf4a65f594dddd97190b6f21e
Madez17 <Madez17>
2019-06-01 00:22:18 +0000

    retrieves a value associated with a key

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -1,30 +1,33 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get -
+ * hash_table_get - Function retrieves a value associated
  *
- * Return: 
+ * @ht: pointer to array
+ * @key: key
+ *
+ * Return: value
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    unsigned long int index;
-    const unsigned char *keynew;
-    hash_node_t *aux;
+unsigned long int index;
+const unsigned char *keynew;
+hash_node_t *aux;
 
-    if (ht == NULL || key == NULL || strlen(key) == 0)
-        return (0);
+if (ht == NULL || key == NULL || strlen(key) == 0)
+return (0);
 
-    keynew = (const unsigned char *)key;
-    index = key_index(keynew, ht->size);
-    aux = ht->array[index];
-   
-    while (aux != NULL)
-    {
-        if (strcmp(key,aux->key) == 0)
-        {
-            return (aux->value);
-        }
-        aux = aux->next;
-    }
-    return (NULL); 
+keynew = (const unsigned char *)key;
+index = key_index(keynew, ht->size);
+aux = ht->array[index];
+
+while (aux != NULL)
+{
+if (strcmp(key, aux->key) == 0)
+{
+return (aux->value);
+}
+aux = aux->next;
+}
+return (NULL);
 }
