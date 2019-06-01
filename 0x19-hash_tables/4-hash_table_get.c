fd8b9ec17f2d8e9d5f2be15f5452bdd4c8cd339e
Madez17 <Madez17>
2019-06-01 00:18:19 +0000

    retrieves a value associated with a key

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -7,5 +7,24 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+    unsigned long int index;
+    const unsigned char *keynew;
+    hash_node_t *aux;
 
+    if (ht == NULL || key == NULL || strlen(key) == 0)
+        return (0);
+
+    keynew = (const unsigned char *)key;
+    index = key_index(keynew, ht->size);
+    aux = ht->array[index];
+   
+    while (aux != NULL)
+    {
+        if (strcmp(key,aux->key) == 0)
+        {
+            return (aux->value);
+        }
+        aux = aux->next;
+    }
+    return (NULL); 
 }
