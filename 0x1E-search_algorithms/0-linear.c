87638b3d6f1d7014d650d14b88cf03ac28b3c399
Madez <781@holbertonschool.com>
2019-08-28 21:01:04 +0000

    [Fixed] -  Betty syntax

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -4,7 +4,7 @@
  * linear_search - Function that searches for a value in an array of integers
  *
  * @array: pointer to the first element
- * @size: size of array  
+ * @size: size of array
  * @value: value
  *
  * Return: success
@@ -16,13 +16,12 @@ int linear_search(int *array, size_t size, int value)
 	for (iter = 0; iter < size; iter++)
 	{
 		if (array == NULL)
-			return -1;
+			return (-1);
 
 		printf("Value checked array[%d] = [%d]\n", (int)iter, array[iter]);
-		
+
 		if (array[iter] == value)
 			return ((int)iter);
 	}
-	return -1;
-		
+	return (-1);
 }
